05c2623688fac9fdb122735f763c1b819bab2f00
Gerald Valdez <miso.clsu@gmail.com>
2023-03-09 17:00:33 +0800

    update schedule modal

--- assets/charts/administrator-chart.js
+++ assets/charts/administrator-chart.js
@@ -241,6 +241,7 @@ function enrollPerCourse()
     
     var yAxis = chart.yAxes.push(
         am5xy.CategoryAxis.new(root, {
+            start: 0.1,
             maxDeviation: 0,
             categoryField: "course",
             renderer: yRenderer
@@ -333,45 +334,51 @@ function enrollPerCourse()
             // https://www.amcharts.com/docs/v5/concepts/animations/
             series.appear(1000);
             chart.appear(1000, 100);
+
+            enrollPerCoursePie();
         }
     });
-    
-    
 }enrollPerCourse();
 
 function enrollPerCoursePie()
 {
-    // Create root element
-    // https://www.amcharts.com/docs/v5/getting-started/#Root_element
+
+    if(MyGlobalObject["enrollPerCoursePie"])
+    {   //check if exist chart dispose that
+        MyGlobalObject["enrollPerCoursePie"].dispose()
+    }
+    // // Create root element
+    // // https://www.amcharts.com/docs/v5/getting-started/#Root_element
     var root = am5.Root.new("enrollPerCoursePie");
 
+    MyGlobalObject["enrollPerCoursePie"]=root;
 
     // Set themes
     // https://www.amcharts.com/docs/v5/concepts/themes/
     root.setThemes([
-    am5themes_Animated.new(root)
+        am5themes_Animated.new(root)
     ]);
 
 
     // Create chart
     // https://www.amcharts.com/docs/v5/charts/percent-charts/pie-chart/
     var chart = root.container.children.push(am5percent.PieChart.new(root, {
-    layout: root.verticalLayout
+        layout: root.verticalLayout
     }));
 
 
     // Create series
     // https://www.amcharts.com/docs/v5/charts/percent-charts/pie-chart/#Series
     var series = chart.series.push(am5percent.PieSeries.new(root, {
-    alignLabels: true,
-    calculateAggregates: true,
-    valueField: "value",
-    categoryField: "category"
+        alignLabels: true,
+        calculateAggregates: true,
+        valueField: "value",
+        categoryField: "course"
     }));
 
     series.slices.template.setAll({
-    strokeWidth: 3,
-    stroke: am5.color(0xffffff)
+        strokeWidth: 3,
+        stroke: am5.color(0xffffff)
     });
 
     series.labelsContainer.set("paddingTop", 30)
@@ -380,56 +387,75 @@ function enrollPerCoursePie()
     // Set up adapters for variable slice radius
     // https://www.amcharts.com/docs/v5/concepts/settings/adapters/
     series.slices.template.adapters.add("radius", function (radius, target) {
-    var dataItem = target.dataItem;
-    var high = series.getPrivate("valueHigh");
+        var dataItem = target.dataItem;
+        var high = series.getPrivate("valueHigh");
 
-    if (dataItem) {
-        var value = target.dataItem.get("valueWorking", 0);
-        return radius * value / high
-    }
-    return radius;
+        if (dataItem) {
+            var value = target.dataItem.get("valueWorking", 0);
+            return radius * value / high
+        }
+        return radius;
     });
 
 
     // Set data
     // https://www.amcharts.com/docs/v5/charts/percent-charts/pie-chart/#Setting_data
-    series.data.setAll([{
-    value: 10,
-    category: "One"
-    }, {
-    value: 9,
-    category: "Two"
-    }, {
-    value: 6,
-    category: "Three"
-    }, {
-    value: 5,
-    category: "Four"
-    }, {
-    value: 4,
-    category: "Five"
-    }, {
-    value: 3,
-    category: "Six"
-    }]);
+    var data = [];
+
+    // series.data.setAll([{
+    // value: 10,
+    // category: "One"
+    // }, {
+    // value: 9,
+    // category: "Two"
+    // }, {
+    // value: 6,
+    // category: "Three"
+    // }, {
+    // value: 5,
+    // category: "Four"
+    // }, {
+    // value: 4,
+    // category: "Five"
+    // }, {
+    // value: 3,
+    // category: "Six"
+    // }]);
+
+    $.ajax({
+        url: window.location.origin + "/office-of-admissions/administrator/enrollPerCourse",
+        type: "POST",
+        data: { sem: $("#enrollPerSemFilter option:selected").val(), college: $("#enrollPerSemCollegeFilter option:selected").val() },
+        dataType: "JSON",
+        success: function(response) 
+        {
+            for (let index = 0; index < response.length; index++) 
+            {
+                data.push(response[index]);
+            }
+
+            series.data.setAll(data);
+            
+            legend.data.setAll(series.dataItems);
+
+            // Play initial series animation
+            // https://www.amcharts.com/docs/v5/concepts/animations/#Animation_of_series
+            series.appear(1000, 100);
+        }
+    });
 
 
     // Create legend
     // https://www.amcharts.com/docs/v5/charts/percent-charts/legend-percent-series/
     var legend = chart.children.push(am5.Legend.new(root, {
-    centerX: am5.p50,
-    x: am5.p50,
-    marginTop: 15,
-    marginBottom: 15
+        centerX: am5.p50,
+        x: am5.p50,
+        marginTop: 15,
+        marginBottom: 15
     }));
 
-    legend.data.setAll(series.dataItems);
-
-
-    // Play initial series animation
-    // https://www.amcharts.com/docs/v5/concepts/animations/#Animation_of_series
-    series.appear(1000, 100);
-}enrollPerCoursePie();
+    
+}
   
 function handleHover(dataItem, orientation) 
 {
--- assets/pages/administrator.js
+++ assets/pages/administrator.js
@@ -2,6 +2,63 @@
 $(document).ready(function(){
 
    let scheduleData = $('#scheduleTable').DataTable({
+      'dom': 'lBfrtip',
+      'buttons': [
+         {
+             extend: 'excelHtml5',
+             exportOptions: {
+                 columns: [2,3,4,5,6,7,8,9,10,11]
+             },
+             filename: 'EVALUATION TEMPLATE',
+             "action": newexportaction
+         },
+         {
+             extend: 'pdfHtml5',
+             orientation: 'landscape',
+             pageSize: 'LEGAL',
+             messageTop: 'Student Copy',
+             title: 'OFFICE OF ADMISSIONS',
+             exportOptions: {
+                 columns: [2,3,4,5,6,7,8,9,10,11]
+             },
+             "action": newexportaction
+         },
+         {
+             extend: 'print',
+             messageTop: 'Student Copy',
+             title: 'OFFICE OF ADMISSIONS',
+             exportOptions: {
+                 columns: [2,3,4,5,6,7,8,9,10,11]
+             },
+             "action": newexportaction,
+             customize: function(win)
+             {
+     
+                 var last = null;
+                 var current = null;
+                 var bod = [];
+     
+                 var css = '@page { size: landscape; }',
+                     head = win.document.head || win.document.getElementsByTagName('head')[0],
+                     style = win.document.createElement('style');
+     
+                 style.type = 'text/css';
+                 style.media = 'print';
+     
+                 if (style.styleSheet)
+                 {
+                     style.styleSheet.cssText = css;
+                 }
+                 else
+                 {
+                     style.appendChild(win.document.createTextNode(css));
+                 }
+     
+                 head.appendChild(style);
+             },
+             pageSize: 'LEGAL'
+         }
+      ],
       'responsive': true,
       'autoWidth': false,
       'processing': true,
@@ -47,6 +104,47 @@ $(document).ready(function(){
 
    let arrDay = ["M_", "T_", "W_", "TH_", "F_", "S_"]; 
 
+   function newexportaction(e, dt, button, config) {
+      var self = this;
+      var oldStart = dt.settings()[0]._iDisplayStart;
+      dt.one('preXhr', function (e, s, data) {
+          // Just this once, load all data from the server...
+          data.start = 0;
+          data.length = 2147483647;
+          dt.one('preDraw', function (e, settings) {
+              // Call the original action function
+              if (button[0].className.indexOf('buttons-copy') >= 0) {
+                  $.fn.dataTable.ext.buttons.copyHtml5.action.call(self, e, dt, button, config);
+              } else if (button[0].className.indexOf('buttons-excel') >= 0) {
+                  $.fn.dataTable.ext.buttons.excelHtml5.available(dt, config) ?
+                      $.fn.dataTable.ext.buttons.excelHtml5.action.call(self, e, dt, button, config) :
+                      $.fn.dataTable.ext.buttons.excelFlash.action.call(self, e, dt, button, config);
+              } else if (button[0].className.indexOf('buttons-csv') >= 0) {
+                  $.fn.dataTable.ext.buttons.csvHtml5.available(dt, config) ?
+                      $.fn.dataTable.ext.buttons.csvHtml5.action.call(self, e, dt, button, config) :
+                      $.fn.dataTable.ext.buttons.csvFlash.action.call(self, e, dt, button, config);
+              } else if (button[0].className.indexOf('buttons-pdf') >= 0) {
+                  $.fn.dataTable.ext.buttons.pdfHtml5.available(dt, config) ?
+                      $.fn.dataTable.ext.buttons.pdfHtml5.action.call(self, e, dt, button, config) :
+                      $.fn.dataTable.ext.buttons.pdfFlash.action.call(self, e, dt, button, config);
+              } else if (button[0].className.indexOf('buttons-print') >= 0) {
+                  $.fn.dataTable.ext.buttons.print.action(e, dt, button, config);
+              }
+              dt.one('preXhr', function (e, s, data) {
+                  // DataTables thinks the first item displayed is index 0, but we're not drawing that.
+                  // Set the property to what it was before exporting.
+                  settings._iDisplayStart = oldStart;
+                  data.start = oldStart;
+              });
+              // Reload the grid with the original page. Otherwise, API functions like table.cell(this) don't work properly.
+              setTimeout(dt.ajax.reload, 0);
+              // Prevent rendering of the full data to the DOM
+              return false;
+          });
+      });
+      // Requery the server with the new one-time export settings
+      dt.ajax.reload();
+   }
 });
 
 function updateSchedule(schedid, semester) 
